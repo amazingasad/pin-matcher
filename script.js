546f4354ce863fb38362e6cdfa8cee1d6f81e98f
amazingasad <asadme631@gmail.com>
2021-09-19 20:11:16 +0600

    Fixed try left section

--- script.js
+++ script.js
@@ -3,36 +3,44 @@ const generatePinField = document.getElementById("random-pin-box");
 let pinInputField = document.getElementById("pin-input-field");
 let buttons = Array.from(document.getElementsByClassName("button"));
 generateButton.addEventListener("click", function(){
-    let randomPin = Math.random() * 9999;
+    let randomPin = 1000 + Math.random() * 9000;
     let pinNumber = Math.round(randomPin);
-    generatePinField.value = pinNumber;
+    generatePinField.innerText = pinNumber;
 })
 buttons.map( button => {
     button.addEventListener("click", (e) => {
         switch(e.target.innerText){
             case "C":
-                pinInputField.value = "";
+                pinInputField.innerText = "";
                 break;
             case "←":
-                pinInputField.value = pinInputField.value.slice(0, -1);
+                pinInputField.value = pinInputField.innerText.slice(0, -1);
                 break;
             default:
-                pinInputField.value += e.target.innerText;
+                pinInputField.innerText += e.target.innerText;
         }
     })
 });
 const success = document.getElementById("success");
 const failed = document.getElementById("failed");
 const submitButton = document.getElementById("submit-btn");
-let number1 = parseFloat(generatePinField.value);
-let number2 = parseFloat(pinInputField.value);
+let tryLeft = parseInt(document.getElementById("try-left").innerText);
+let number1 = generatePinField.innerText;
+let number2 = pinInputField.innerText;
 submitButton.addEventListener("click", function(){
     if(number1 == number2){
         success.style.display = "block";
         alert("Pin matched");
+        console.log(number1);
+        console.log(number2);
     }
     else{
         failed.style.display = "block";
-        alert("Pin didn't match");
+        tryLeft = tryLeft -1;
+        document.getElementById("try-left").innerText = tryLeft;
+        if(tryLeft < 0){
+            document.getElementById("try-left").innerText = 0;
+            alert("tries finished");
+        }
     }
 })
